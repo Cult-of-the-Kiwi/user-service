46791297f9f845ac94f64ad8e10b052816197de4
Lamoara <jorginos40@gmail.com>
2026-01-06 12:35:30 +0100

    Old code cleanup

--- src/domain/models/user.rs
+++ src/domain/models/user.rs
@@ -8,17 +8,11 @@ use sqlx::FromRow;
 
 use crate::domain::types::{Time, UserID, Username};
 
-#[derive(FromRow, Debug, Default, Deserialize, Serialize, Clone)]
+#[derive(FromRow, Debug, Default, Deserialize, Serialize, Clone, PartialEq, Eq)]
 pub(crate) struct User {
     pub username: Username,
     pub id: UserID,
     pub created_at: Option<Time>,
-}
-
-impl PartialEq for User {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
-    }
 }
 
 impl IntoResponse for User {
